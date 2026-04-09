585c84b0cb2693d2d9563baccd13f9ac49e0d1cd
Atharva Chothe <chotheatharva007@gmail.com>
2026-04-09 15:17:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ from dotenv import load_dotenv
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="sphurtin_org_chart_db"
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
 )
 
 cursor = db.cursor()
